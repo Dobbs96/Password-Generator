a509f9104898a4e4c6b168f186f9f24efaf38465
James Dobbs <dobbs282@gmail.com>
2021-07-15 18:10:56 -0400

    changed order arounf to ad noPassword

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var generateBtnNO = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
+  var noPassword = generateNoPassword();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  passwordText.value = noPassword;
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+generateBtnNO.addEventListener("click", writePassword);
 
+// quick tpyed vars
 var num = "0123456789";
 var lc = "abcdefghijklmnopqrstuvwxyz";
 var special = "`~!@#$%^&*()_+-=[]}|;'/.,?><";
 
+// create vars used to find value
 var numbers = num.split("");
 var lowercase = lc.split("");
 var specialCharacters = special.split("");
@@ -27,17 +32,53 @@ console.log(lowercase);
 console.log(specialCharacters);
 console.log(uppercase);
 
-password.addEventListener("click", function () {
-  document.getElementById("password");
-});
-
+// vars for messages on windows screen
 var ok;
 var confirmLowercase;
 var confirmUppercase;
 var confirmSpecialCharacters;
 var confirmNumber;
+// need a choice selector
+var choice;
 
+function generateNoPassword() {
+  // 1
+  confirmLowercase = confirm("Will it contain Lowercase letters?");
+  // 2
+  confirmUppercase = confirm("Will it contain Uppercase letters?");
+  // 3
+  confirmSpecialCharacters = confirm("Will it contain Special Characters?");
+  // 4
+  confirmNumber = confirm("Will it contain Numbers?");
+
+  // if 1,2,3,4 are cancelled
+  if (
+    !confirmLowercase &&
+    !confirmUppercase &&
+    !confirmSpecialCharacters &&
+    !confirmNumber
+  ) {
+    choice = alert("You must selected a criteria!");
+  }
+}
 function generatePassword() {
+  // if 1 selected
+
+  // if 2 selected
+
+  // if 3 selected
+
+  // if 4 all four selected
+  if (
+    confirmLowercase &&
+    confirmUppercase &&
+    confirmSpecialCharacters &&
+    confirmNumber
+  ) {
+    choice = lowercase.concat(uppercase, specialCharacters, numbers);
+    console.log(choice);
+  }
+
   ok = parseInt(
     prompt(
       "How many characters would you like your password? *Please choose between 8 and 128*"
@@ -46,11 +87,6 @@ function generatePassword() {
   if (!ok) {
     alert("Please try again.");
   } else if (ok < 8 || ok > 128) {
-    alert("Value must be between 8 and 128");
-  } else if (ok >= 8 || ok <= 128) {
-    confirmLowercase = confirm("Will this contain numbers?");
-    confirmUppercase = confirm("Will this contain special characters?");
-    confirmSpecialCharacters = "Will this contain Uppercase letters?";
-    confirmNumber = confirm("Will this contain Lowercase letters?");
+    alert("Value must be between 8 and 128!");
   }
 }
